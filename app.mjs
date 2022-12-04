e9e387100be1d9ca1269620d02256c92653acbde
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-12-04 19:50:36 +0100

    Worked on the app

---
+++ app.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import userRoute from './routes/userRoute.mjs';
+import errorHandler from './controllers/errorController.mjs';
+import AppError from './utils/appError.mjs';
+
+const app = express();
+
+if (process.env.NODE_ENV === 'dev') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/users', userRoute);
+
+//Fallback route
+app.use((req, res, next) => {
+  next(new AppError('Page not found', 404));
+});
+
+//Error Handler
+app.use(errorHandler);
+
+export default app;
