326791c79ca0129ba783410cc7cec426db5529e1
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-12-04 19:51:19 +0100

    Worked on the error controller

---
+++ controllers/errorController.mjs
@@ -0,0 +1,22 @@
+const sendErrDev = (err, req, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    statusCode: err.statusCode,
+    message: err.message
+  });
+};
+
+export default (err, req, res, next) => {
+  console.log('Error Handler triggered');
+  const error = { ...err };
+  error.statusCode = err.statusCode || 500;
+  error.status = err.status || 'fail';
+  error.message = err.message || 'Internal Server Error';
+  console.log(error);
+  if (process.env.NODE_ENV === 'dev') {
+    //App running in dev env
+    sendErrDev(error, req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    //Dev running in production env
+  }
+};
