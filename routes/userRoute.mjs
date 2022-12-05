2b9a4b85050e12272d747aaf64af4e28bc1b5444
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-12-05 21:04:06 +0100

    Added login route

--- routes/userRoute.mjs
+++ routes/userRoute.mjs
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
 import * as userController from '../controllers/userController.mjs';
+import * as authController from '../controllers/authController.mjs';
 
 const router = Router();
 
 router
   .route('/')
-  .get(userController.getAllUsers)
+  .get(authController.protect, userController.getAllUsers)
   .post(userController.createUser);
+
+router.post('/login', authController.loginUser);
 
 export default router;
