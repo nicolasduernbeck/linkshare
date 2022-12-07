0e7cf61a8f526e84b568ce17542fb6a2b492822e
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-12-08 00:05:56 +0100

    Updated viewController

--- controllers/viewController.mjs
+++ controllers/viewController.mjs
@@ -5,8 +5,8 @@ import AppError from '../utils/appError.mjs';
 export const renderLinks = catchError(async (req, res, next) => {
   console.log(req.params);
   const user = await User.findOne({ slug: req.params.slug });
-  if (!user) return next(new AppError('Page not foun', 404));
-  res.status(200).render('links', { links: user.links });
+  if (!user) return next(new AppError('Page not found', 404));
+  res.status(200).render('links', { name: user.name, links: user.links });
 });
 
 export const renderLogin = catchError(async (req, res, next) => {});
