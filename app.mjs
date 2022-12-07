ca432896da57a6a7b58f175929363b379c3b8afb
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2022-12-08 00:05:40 +0100

    Added static folder 📂

--- app.mjs
+++ app.mjs
@@ -14,6 +14,7 @@ import AppError from './utils/appError.mjs';
 const app = express();
 
 app.use(express.json());
+app.use(express.static(`public`));
 app.set('view engine', 'pug');
 app.use(xss());
 app.use(mongoSanitize());
